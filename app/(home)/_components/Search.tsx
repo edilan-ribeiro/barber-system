e28bac41cfb4e4202377cd6f636e4c43bf92c602
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-01-31 18:36:32 -0300

    feat: added initial booking component

--- app/(home)/_components/Search.tsx
+++ app/(home)/_components/Search.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/app/_components/ui/button'
 import { Input } from '@/app/_components/ui/input'
 import { SearchIcon } from 'lucide-react'
 
-const Search = () => {
+export const Search = () => {
 	return (
 		<div className="flex items-center gap-2">
 			<Input placeholder='Busque por uma barbearia...'/>
@@ -15,4 +15,3 @@ const Search = () => {
 	)
 }
 
-export default Search
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,8 @@
 import { format } from 'date-fns'
 import { Header } from '../_components/Header'
 import { ptBR } from 'date-fns/locale/pt-BR'
-import Search from './_components/Search'
+import { Search } from './_components/Search'
+import { Booking } from '../_components/Booking'
 
 export default function Home() {
 	return (
@@ -17,6 +18,11 @@ export default function Home() {
 			</div>
 			<div className="px-5 mt-6">
 				<Search />
+			</div>
+
+			<div className="px-5 mt-6">
+				<h2 className='text-xs uppercase text-gray-400 font-bold mb-3'>Agendamentos</h2>
+				<Booking/>
 			</div>
 		</div>
 	)
