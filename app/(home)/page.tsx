3b7acc04de648e89203f9caf75c40a55efd2c024
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-02-08 21:39:12 -0300

    feat: added username to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -34,7 +34,9 @@ export default async function Home() {
 		<div>
 			<Header />
 			<div className="px-5 pt-5">
-				<h2 className="text-xl font-bold">Olá, fulano!</h2>
+				<h2 className="text-xl font-bold">
+					{session?.user ? `Olá, ${session.user.name?.split(' ')[0]}` : 'Olá! Vamos fazer o seu agendamento?'}
+				</h2>
 				<p className="capitalize text-sm">
 					{format(new Date(), "EEEE ',' dd 'de' MMMM", {
 						locale: ptBR,
