5ae0ec5289035eac4e7d78bf9b59756260e6256c
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-02-09 11:37:16 -0300

    fix: ensure firs avaiable time to book be after the current time

--- app/barbershops/[id]/_helpers/hour.ts
+++ app/barbershops/[id]/_helpers/hour.ts
@@ -1,16 +1,20 @@
-import { addMinutes, format, setHours, setMinutes } from 'date-fns'
+import { addMinutes, format, isAfter, setHours, setMinutes } from 'date-fns'
 
 export function generateDayTimeList(date: Date): string[] {
+	const currentTime = addMinutes(new Date(), 60)//avoids the booking to be placed too close by the customer
 	const startTime = setMinutes(setHours(date, 9), 0) //set initial time to 9:00
-	const endTime = setMinutes(setHours(date, 21), 0) //set final time to 21:00
+	const endTime = setMinutes(setHours(date, 17), 0) //set final time to 17:00 so shops can close with the final booking
 	const interval = 45 //interval in minutes
-	const timeList: string[] = [] //holds all possible times
+	const timeList: string[] = [] //holds all possible booking times
 
-	let currentTime = startTime
+	let nextAvaiableTime = startTime
 
-	while (currentTime < endTime) {
-		timeList.push(format(currentTime, 'HH:mm'))
-		currentTime = addMinutes(currentTime, interval)
+	while (nextAvaiableTime < endTime) {
+		if (isAfter(nextAvaiableTime, currentTime)) {
+			//  checks if the booking times are after the current time
+			timeList.push(format(nextAvaiableTime, 'HH:mm'))
+		}
+		nextAvaiableTime = addMinutes(nextAvaiableTime, interval)
 	}
 
 	return timeList
