90f4ba3da8e2555126f994afde7aeced06164929
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-02-28 13:08:27 -0300

    feat: added desktop mode to bookings page

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -61,41 +61,45 @@ const BookingsPage = async () => {
   ]);
 
   return (
-    <>
-      <div className="px-5 py-6">
-        <h1 className="mb-6 text-xl font-bold">Agendamentos</h1>
+    <div className="lg:flex lg:w-full lg:justify-center lg:px-32">
+      <div className="px-5 py-6 lg:mb-10 lg:w-1/2">
+        <div>
+          <h1 className="mb-6 text-xl font-bold">Agendamentos</h1>
 
-        {confirmedBookings.length > 0 && (
-          <>
-            <h2 className="mb-3 text-sm font-bold uppercase text-gray-400">
-              Confirmados
-            </h2>
-            <div className="flex flex-col gap-3">
-              {confirmedBookings.map(
-                (booking: { id: Key | null | undefined }) => (
-                  <BookingItem booking={booking} key={booking.id} />
-                ),
-              )}
-            </div>
-          </>
-        )}
+          {confirmedBookings.length > 0 && (
+            <>
+              <h2 className="mb-3 text-sm font-bold uppercase text-gray-400">
+                Confirmados
+              </h2>
+              <div className="flex flex-col gap-3">
+                {confirmedBookings.map(
+                  (booking: { id: Key | null | undefined }) => (
+                    <BookingItem booking={booking} key={booking.id} />
+                  ),
+                )}
+              </div>
+            </>
+          )}
+        </div>
 
-        {finishedBookings.length > 0 && (
-          <>
-            <h2 className="mb-3 mt-6 text-sm font-bold uppercase text-gray-400">
-              Finalizados
-            </h2>
-            <div className="flex flex-col gap-3">
-              {finishedBookings.map(
-                (booking: { id: Key | null | undefined }) => (
-                  <BookingItem booking={booking} key={booking.id} />
-                ),
-              )}
-            </div>
-          </>
-        )}
+        <div>
+          {finishedBookings.length > 0 && (
+            <>
+              <h2 className="mb-3 mt-6 text-sm font-bold uppercase text-gray-400">
+                Finalizados
+              </h2>
+              <div className="flex flex-col gap-3">
+                {finishedBookings.map(
+                  (booking: { id: Key | null | undefined }) => (
+                    <BookingItem booking={booking} key={booking.id} />
+                  ),
+                )}
+              </div>
+            </>
+          )}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
